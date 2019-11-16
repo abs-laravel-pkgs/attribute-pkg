825c891ea95b1df2d45ca090300d0705dc50822a
Abdul <abdul@uitouxsolutions.com>
2019-11-16 11:53:00 +0530

    eav

---
+++ src/migrations/2019_11_16_113823_eav_text_c.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class EavTextC extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('eav_text', function (Blueprint $table) {
+			$table->unsignedInteger('entity_type_id');
+			$table->unsignedInteger('entity_id');
+			$table->unsignedInteger('field_group_id');
+			$table->unsignedInteger('field_id');
+			$table->text('value');
+
+			$table->foreign('entity_type_id')->references('id')->on('configs')->onDelete('CASCADE')->onUpdate('cascade');
+
+			$table->foreign('field_group_id')->references('id')->on('field_groups')->onDelete('CASCADE')->onUpdate('cascade');
+			$table->foreign('field_id')->references('id')->on('fields')->onDelete('CASCADE')->onUpdate('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('eav_text');
+	}
+}
