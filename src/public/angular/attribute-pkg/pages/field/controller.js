2ae77ce98515fd253b377d01d0af807f8bf334a6
Abdul <abdul@uitoux.in>
2020-01-13 11:52:32 +0530

    u

--- src/public/angular/attribute-pkg/pages/field/controller.js
+++ src/public/angular/attribute-pkg/pages/field/controller.js
@@ -1,3 +1,35 @@
+app.config(['$routeProvider', function($routeProvider) {
+
+    $routeProvider.
+    //FIELDS
+    when('/attribute-pkg/field/list/:category_id', {
+        template: '<field-list></field-list>',
+        title: 'Fields',
+    }).
+    when('/attribute-pkg/field/add/:category_id', {
+        template: '<field-form></field-form>',
+        title: 'Add Field',
+    }).
+    when('/attribute-pkg/field/edit/:category_id/:id', {
+        template: '<field-form></field-form>',
+        title: 'Edit Field',
+    }).
+
+    //FIELD GROUPS
+    when('/attribute-pkg/field-group/list/:category_id', {
+        template: '<field-group-list></field-group-list>',
+        title: 'Field Groups',
+    }).
+    when('/attribute-pkg/field-group/add/:category_id', {
+        template: '<field-group-form></field-group-form>',
+        title: 'Add Field Group',
+    }).
+    when('/attribute-pkg/field-group/edit/:category_id/:id', {
+        template: '<field-group-form></field-group-form>',
+        title: 'Edit Field Group',
+    });
+}]);
+
 app.component('fieldList', {
     templateUrl: field_list_template_url,
     controller: function($http, $location, HelperService, $scope, $routeParams, $rootScope, $element) {
---
+++ src/views/setup.blade.php
@@ -0,0 +1,28 @@
+@if(config('custom.PKG_DEV'))
+    <?php $attribute_pkg_prefix = '/packages/abs/attribute-pkg/src';?>
+@else
+    <?php $attribute_pkg_prefix = '';?>
+@endif
+
+<!-- FIELD -->
+<script type="text/javascript">
+    var field_get_filter_data_url = "{{route('getFieldFilterdata')}}";
+    var field_list_template_url = "{{URL::asset($attribute_pkg_prefix.'/public/angular/attribute-pkg/pages/field/list.html')}}";
+    var field_list_url = "{{url('#!/attribute-pkg/field/list')}}";
+    var field_form_template_url = "{{URL::asset($attribute_pkg_prefix.'/public/angular/attribute-pkg/pages/field/form.html')}}";
+    var field_get_form_data_url = "{{url('/attribute-pkg/field/get-form-data')}}";
+    var field_delete_url = "{{url('/attribute-pkg/field/delete')}}";
+    var field_form_url = "{{url('#!/attribute-pkg/field/add')}}";
+</script>
+<script type="text/javascript" src="{{URL::asset($attribute_pkg_prefix.'/public/angular/attribute-pkg/pages/field/controller.js?v=2')}}"></script>
+
+<script type="text/javascript">
+    var field_group_get_filter_data_url = "{{route('getFieldGroupFilterdata')}}";
+    var field_group_list_template_url = "{{URL::asset($attribute_pkg_prefix.'/public/angular/attribute-pkg/pages/field-group/list.html')}}";
+    var field_group_list_url = "{{url('#!/attribute-pkg/field-group/list')}}";
+    var field_group_form_template_url = "{{URL::asset($attribute_pkg_prefix.'/public/angular/attribute-pkg/pages/field-group/form.html')}}";
+    var field_group_get_form_data_url = "{{url('/attribute-pkg/field-group/get-form-data')}}";
+    var field_group_delete_url = "{{url('/attribute-pkg/field-group/delete')}}";
+    var field_group_form_url = "{{url('#!/attribute-pkg/field-group/add')}}";
+</script>
+<script type="text/javascript" src="{{URL::asset($attribute_pkg_prefix.'/public/angular/attribute-pkg/pages/field-group/controller.js?v=2')}}"></script>
