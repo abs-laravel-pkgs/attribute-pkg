de7921e5b63a9aa1f8c89ebff4b3f836b00f2c22
Abdul <abdul@uitoux.in>
2020-01-27 11:42:11 +0530

    u

--- src/AttributePkgServiceProvider.php
+++ src/AttributePkgServiceProvider.php
@@ -17,6 +17,8 @@ class AttributePkgServiceProvider extends ServiceProvider {
 		$this->loadViewsFrom(__DIR__ . '/views', 'attribute-pkg');
 		$this->publishes([
 			__DIR__ . '/public' => base_path('public'),
+			__DIR__ . '/database/seeds/client' => 'database/seeds',
+			__DIR__ . '/config/config.php' => config_path('attribute-pkg.php'),
 		]);
 	}
 
